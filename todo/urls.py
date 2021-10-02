028a5be7ac9c387936a6611734aa364c69381339
unknown <khuongduy354@gmail.com>
2021-10-02 15:51:06 +0700

    link index.html to url

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path,re_path
+from django.views.generic import TemplateView
 from .views import TodoView
 
 
 urlpatterns = [
     path('todos',TodoView.as_view()),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
