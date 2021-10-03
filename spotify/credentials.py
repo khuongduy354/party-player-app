50f811a43caa08d0ebb38e751be179fbd1c993f3
unknown <khuongduy354@gmail.com>
2021-10-03 21:24:42 +0700

    change redirecturl

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID='5a16a79fd6d14597921c7568a0459f35'
 CLIENT_SECRET='596de3f1b97a435a9b126776d9a9520b'
-REDIRECT_URI='http://127.0.0.1:8000/spotify/redirect'
+REDIRECT_URI='https://deploy-test-django-react.herokuapp.com/spotify/redirect'
