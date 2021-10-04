917a7f84261525834616da879634a322a7fb4698
unknown <khuongduy354@gmail.com>
2021-10-04 08:32:06 +0700

    hotfix

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID='5a16a79fd6d14597921c7568a0459f35'
 CLIENT_SECRET='596de3f1b97a435a9b126776d9a9520b'
-REDIRECT_URI='https://deploy-test-django-react.herokuapp.com/spotify/redirect'
+REDIRECT_URI='https://deploy-test-django-react.herokuapp.com/spotify/redirect/'
