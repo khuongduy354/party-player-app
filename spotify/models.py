18b1ea5ce5d4ec5ba97041c2b310e9a812e5caf2
unknown <khuongduy354@gmail.com>
2021-10-04 08:47:19 +0700

    lengthen model

--- spotify/models.py
+++ spotify/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class SpotifyToken(models.Model):
     user = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    refresh_token = models.CharField(max_length=150)
-    access_token = models.CharField(max_length=150)
+    refresh_token = models.CharField(max_length=200)
+    access_token = models.CharField(max_length=200)
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length=50)
